4438b95625aace75cca86576eb3c83362f4f3585
rohit gupta <r04itgupta@gmail.com>
2022-02-15 18:22:16 +0530

    adding put request in admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -81,6 +81,45 @@ router.post("/:year/:branch/:sem/:material", async (req, res) => {
 })
 
 
+router.put("/:year/:branch/:sem/:material/:subject", async (req, res) => {
+
+    const year = Number(req.params.year);
+    const branch = req.params.branch;
+    const sem = Number(req.params.sem);
+    const material = req.params.material;
+    const subject = req.params.subject;
+
+    let course = {year:year, branch:branch, sem:sem,material:material};
+    
+    const {error} = validateCourse(course);
+    if(error) return res.status(400).send(error.message)
+
+
+    let validbody = validateBody(req.body)
+    if(validbody.error) return res.status(400).send(`${validbody.error}`);
+    
+    course = await College.findOne({ branch: branch, year: year })
+
+    let found = course.semester[sem][material];
+    let foundIndex = -1;
+
+    for (let i=0; i<found.length; i++){
+
+        if(found[i]["subjectName"].trim()===subject){
+            foundIndex = i;
+        }
+    }
+    
+    if(foundIndex ===-1) {return res.send(`Not founded ${foundIndex}`)}
+
+    course.semester[sem][material][foundIndex]["subjectName"] = req.body.subjectName;
+    course.semester[sem][material][foundIndex]["subjectLink"] = (!req.body.subjectLink)?course.semester[sem][material][foundIndex]["subjectLink"]:req.body.subjectLink;
+
+    await course.save();
+    res.send(course.semester[sem][material])
+})
+
+
 
 
 module.exports = router;
